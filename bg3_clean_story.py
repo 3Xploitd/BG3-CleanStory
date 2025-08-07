dbaa6ab8524b78eafd7bf430195381cf4f6db510
3Xploitd <40121116+3Xploitd@users.noreply.github.com>
2025-08-06 23:26:51 -0500

    Update bg3_clean_story.py

--- bg3_clean_story.py
+++ bg3_clean_story.py
@@ -3,24 +3,25 @@ import os
 import shutil
 import argparse
 import json
+from time import time
 
-def bg3_clean_story(bg3_directory,directive):
-    mods_directory = f'{bg3_directory}\\Mods\\'
+def bg3_clean_story(config,mod):
+    mods_directory = f'{config}\\Mods\\'
     mod_list = []
-    if directive.upper() == 'ALL':
+    if mod.upper() == 'ALL':
         try:
-            for mod in Path(mods_directory).glob('*'):
-                if mod.name in ['Gustav','Shared','SharedDev','GustavDev','GustavX']:
+            for mods in Path(mods_directory).glob('*'):
+                if mods.name in ['Gustav','Shared','SharedDev','GustavDev','GustavX']:
                     continue
                 else:
-                    mod_list.append(mod)
+                    mod_list.append(mods)
             
         except Exception as e:
             print('Something went wrong when trying gather the list of mods to clean the story files from. Check the specified BG3 directory for errors.')
 
-    if directive.upper() != 'ALL':
+    if mod.upper() != 'ALL':
         try:
-            mod_list.append(f'{bg3_directory}\\Mods\\{directive}')
+            mod_list.append(f'{config}\\Mods\\{mod}')
         except Exception as e:
             print('Something went wrong trying to locate the specified mod, check the specified modname and bg3 directory for correctness')
     return mods_directory,mod_list
@@ -55,13 +56,30 @@ def bg3_delete_story_files(mods_directory,mod_list):
             continue
         if deleted_files != []:
             mods_json['mods'][mod_name.name] = deleted_files
-    print(json.dumps(mods_json,indent=4))
+    logfile = f'storyfiles_cleaned_{time()}'
+    
+    if len(mods_json) != 0:
+
+        with open(logfile,'a') as fh:
+            fh.write(json.dumps(mods_json,indent=4))
+            fh.close()
+        print(f'Deleted files logged at {Path(logfile)}')
+    else:
+        print('No bloated story files were detected. No files were deleted.')
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-s','--source-directory',help='The full path of your BG3 Data directory',action='store',default='C:\\Program Files (x86)\\Steam\\steamapps\\common\\Baldurs Gate 3\\Data\\')
-    parser.add_argument('-d','--directive',help='The mod name you want to remove bloated story files from, if you want to do it for all mods then specify "ALL"',action='store',default='ALL')
+    parser.add_argument('-m','--mod',help='The mod name (the entire modname ex.TESTMOD_67d6dc31-b234-46fb-b8f9-11611b7a16bb) you want to remove bloated story files from. If you want to do it for all mods then specify "ALL" or omit this comandline option',action='store',default='ALL')
     args = parser.parse_args()
-    mod_directory,mod_list = bg3_clean_story(args.source_directory,args.directive)
+    if Path('config.json').is_file():
+        config = json.loads(open('config.json','r').read())['BG3_Directory']
+    else:
+        config = args.source_directory
+
+    mod = args.mod
+    
+    mod_directory,mod_list = bg3_clean_story(config,mod)
     bg3_delete_story_files(mod_directory,mod_list)
 
